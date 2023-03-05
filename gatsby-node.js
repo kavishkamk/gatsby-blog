4a2e8809f4e2b3b798346e96a6e0b2dc18f2e801
kavishkamk <kavishkamadhushan727@gmail.com>
2023-03-05 17:14:10 +0530

    add blog post functions

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const { createFilePath } = require("gatsby-source-filesystem");
+const path = require("path");
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -7,12 +10,50 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
+
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
   })
+
+  return graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  })
 }
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+
+  }
+};
